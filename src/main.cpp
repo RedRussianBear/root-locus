9c8af9e14f650e9dc5ddb5f579982332ad48eda7
Mikhail Khrenov <mkhrenov34@gmail.com>
2023-05-05 20:46:36 -0400

    Changed to heading estimation (integration) and compensation

--- src/main.cpp
+++ src/main.cpp
@@ -4,9 +4,10 @@
 #include <ServoInput.h>
 #include <Servo.h>
 
-#define CH_ZERO1 1490
-#define CH_ZERO2 1480
+#define CH_ZERO1 1485
+#define CH_ZERO2 1485
 #define US_TO_RADPS 0.02
+#define N_SAMPLES 400
 
 Adafruit_MPU6050 imu;
 Adafruit_NeoPixel pixels(1, PIN_NEOPIXEL);
@@ -16,7 +17,9 @@ ServoInputPin<A6> ch2;
 
 int ch1v = CH_ZERO1;
 int ch2v = CH_ZERO2;
-float g_zero = 0;
+double g_zero = 0;
+double heading = 0;
+unsigned long last_time;
 Servo motor_left, motor_right;
 
 void setup() {
@@ -29,8 +32,8 @@ void setup() {
 
     // Set IMU operating characteristics
     imu.setAccelerometerRange(MPU6050_RANGE_4_G);
-    imu.setGyroRange(MPU6050_RANGE_500_DEG);
-    imu.setFilterBandwidth(MPU6050_BAND_5_HZ);
+    imu.setGyroRange(MPU6050_RANGE_250_DEG);
+    imu.setFilterBandwidth(MPU6050_BAND_10_HZ);
 
     motor_left.attach(A0);
     motor_right.attach(A1);
@@ -41,57 +44,56 @@ void setup() {
 
     motor_left.writeMicroseconds(ch1v);
     motor_right.writeMicroseconds(ch2v);
-    delay(3000);
-    sensors_event_t accel, gyro, temp;
 
-    for (int i = 0; i < 300; i++) {
+    sensors_event_t accel, gyro, temp;
+    for (int i = 0; i < N_SAMPLES; i++) {
         imu.getEvent(&accel, &gyro, &temp);
         g_zero += gyro.gyro.x;
         delay(10);
     }
-    g_zero /= 300;
+    g_zero /= N_SAMPLES;
 
     pixels.setPixelColor(0, 255, 0, 255);
     pixels.show();
+    last_time = micros();
 }
 
-double corr = 0;
+
 void loop() {
     sensors_event_t accel, gyro, temp;
     imu.getEvent(&accel, &gyro, &temp);
     double gx = gyro.gyro.x - g_zero;
-    double K = 0.2;
+    double dt = (last_time - micros()) / 1e6;
+    heading += gx * dt;
+    double K = 60;
 
-
-    pixels.setPixelColor(0, 120 + gx * 50, 0, 120 - gx * 50);
+    pixels.setPixelColor(0, 127 + heading * 50, 0, 127 - heading * 50);
     pixels.show();
 
     ch1v = ch1.getPulse();
     ch2v = ch2.getPulse();
-    double rotation_setpoint = (ch1v + ch2v - CH_ZERO1 - CH_ZERO2) * US_TO_RADPS;
+    int rotation_setpoint = ch1v + ch2v - CH_ZERO1 - CH_ZERO2;
 
-    if (abs(ch1v + ch2v - CH_ZERO1 - CH_ZERO2) < 40 && ch2v > CH_ZERO2 + 56) {
-        corr += (K * gx / US_TO_RADPS);
-    } else {
-        corr = 0;
-    }
-//    Serial.print(gx);
-//    Serial.print('\t');
-//    Serial.print(ch1v);
-//    Serial.print('\t');
-//    Serial.print(ch2v);
-//    Serial.print('\t');
-//    Serial.print(ch1v + ch2v - CH_ZERO1 - CH_ZERO2);
-//    Serial.print('\t');
-//    Serial.print(rotation_setpoint);
-//    Serial.print('\t');
-//    Serial.println(corr);
+    if (rotation_setpoint > 40)
+        heading = 0;
 
+    int correction = (int) (-K * heading);
 
-    motor_left.writeMicroseconds(ch1v + corr);
-    motor_right.writeMicroseconds(ch2v + corr);
+    motor_left.writeMicroseconds(ch1v - correction);
+    motor_right.writeMicroseconds(ch2v - correction);
 
     delay(1);
+    //    Serial.print(gx);
+    //    Serial.print('\t');
+    //    Serial.print(ch1v);
+    //    Serial.print('\t');
+    //    Serial.print(ch2v);
+    //    Serial.print('\t');
+    //    Serial.print(ch1v + ch2v - CH_ZERO1 - CH_ZERO2);
+    //    Serial.print('\t');
+    //    Serial.print(rotation_setpoint);
+    //    Serial.print('\t');
+    //    Serial.println(corr);
     // Gyro positive = turning right
     // motor left minus normal
     // motor right plus normal
